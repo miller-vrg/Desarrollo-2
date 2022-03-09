667755b42d411225037b6c57758bfc9b6406233e
miller-vrg <millervargas09@gmail.com>
2022-03-09 17:27:59 -0500

    Se agregan las clases del diagrama

---
+++ ABP/Controlex/src/main/java/co/edu/unisinu/ingsistemas/ds2/datos/Alumno.java
@@ -0,0 +1,47 @@
+
+package co.edu.unisinu.ingsistemas.ds2.datos;
+
+/**
+ *
+ * @author gasler (Miler vargas mola)
+ * 
+ */
+
+public class Alumno {
+    
+private String nombre;
+private int matricula;
+private ControlEscrito controlEscrito;
+private Practica practica;
+
+public Alumno(){}
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public int getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+}
---
+++ ABP/Controlex/src/main/java/co/edu/unisinu/ingsistemas/ds2/datos/ControlEscrito.java
@@ -0,0 +1,71 @@
+
+package co.edu.unisinu.ingsistemas.ds2.datos;
+
+/**
+ *
+ * @author gasler
+ */
+class ControlEscrito {
+    
+private int numeroControl;
+private int numeroPreguntas;
+private String fecha;
+private float nota;
+
+    /**
+     * @return the numeroControl
+     */
+    public int getNumeroControl() {
+        return numeroControl;
+    }
+
+    /**
+     * @param numeroControl the numeroControl to set
+     */
+    public void setNumeroControl(int numeroControl) {
+        this.numeroControl = numeroControl;
+    }
+
+    /**
+     * @return the numeroPreguntas
+     */
+    public int getNumeroPreguntas() {
+        return numeroPreguntas;
+    }
+
+    /**
+     * @param numeroPreguntas the numeroPreguntas to set
+     */
+    public void setNumeroPreguntas(int numeroPreguntas) {
+        this.numeroPreguntas = numeroPreguntas;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the nota
+     */
+    public float getNota() {
+        return nota;
+    }
+
+    /**
+     * @param nota the nota to set
+     */
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+    
+}
